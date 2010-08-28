50a138f2dbd58c854a838b8b617267ba012e0f6f
bcoe <bencoe@gmail.com>
2010-08-28 14:41:17 -0400

    Crawler should work on an arbitrary github account now as long as there is an index.html pointing at tests :)

--- test/unit/example2.js
+++ test/unit/example2.js
@@ -1,15 +1,15 @@
-module('nko');
+module('nko2');
 
-test("test_assertions", function() {
+test("test_assert_2", function() {
     equals(1, 1, "Simple assertion test.");
-    equals(jQuery('#content').html(), "Not Some Content", "Assert that we get the content of a div with an ID.");
+    equals(jQuery('#content').html(), "Not Some Content", "File 2: Assert that we get the content of a div with an ID.");
 });
 
-test("test_jquery", function() {
+test("test_query_2", function() {
     var element = $('<div></div>');
     element.text('Hello World!');
     element.attr('id', 'foobar');
     $('body').append(element);
     var test = $('#foobar');
-    equals($('#foobar').text(), "Hello World!", "Created element with jQuery added it to DOM and pulled it out.");
+    equals($('#foobar').text(), "Hello World!", "File 2: Created element with jQuery added it to DOM and pulled it out.");
 });
