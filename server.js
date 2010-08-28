bbb0ae5321e61ff6c413db559e098fcd90c7cb11
benvinegar <ben@benv.ca>
2010-08-27 21:04:13 -0700

    Playing around; index has a form that POSTs to /tests/

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ app.configure(function(){
     app.use(express.staticProvider(__dirname + '/public'));
 });
 
-app.listen(parseInt(process.env.PORT || 8000), null);
-
 // Index.
 app.get('/', function(req, res) {
     res.render('view/index.ejs', {
@@ -38,15 +36,24 @@ app.get('/', function(req, res) {
             foo: 'Hello World'
         }
     });
-})
+});
+
+app.post('/tests/', function(req, res) {
+  res.render('view/tests/show.html.ejs', {
+  });
+});
 
 // Unit-Testing endpoint.
 app.get('/test', function(requ, res) {
     sandbox.executeTest( 'www.google.com' );
-    
+
     res.render('view/index.ejs', {
         locals: {
             foo: 'Hello World'
         }
     });
 });
+
+var port = process.env.PORT || 8000;
+app.listen(parseInt(port), null);
+console.log('node server listening on ' + port);
