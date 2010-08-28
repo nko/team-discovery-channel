d235cb24770be69ccc3f59adcb2926610a4ed725
bcoe <bencoe@gmail.com>
2010-08-28 12:42:13 -0400

    Unit tests are reasonable now going to move on to building out the interface better. Fixed up a minor issue being caused by couchdb.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var sandbox = require('sandbox');
 // NPM Bundle
 require('./vendor');
 var connect = require('connect');
-var couchdb = require('couchdb');
+var couchdb = require('vendor/couchdb');
 
 var db = couchdb.createClient(5984, 'shodan.couchone.com').db('tests');
 
--- support/jsdom/lib/jsdom/level1/core.js
+++ support/jsdom/lib/jsdom/level1/core.js
@@ -925,6 +925,16 @@ core.Element.prototype = {
     throw new core.DOMException(NOT_FOUND_ERR);
   }, //raises: function(DOMException) {},
 
+  compareDocumentPosition: function( otherNode ) {
+      DOCUMENT_POSITION_DISCONNECTED = 0x01;
+      DOCUMENT_POSITION_PRECEDING = 0x02;
+      DOCUMENT_POSITION_FOLLOWING = 0x04;
+      DOCUMENT_POSITION_CONTAINS = 0x08;
+      DOCUMENT_POSITION_CONTAINED_BY = 0x10;
+
+      return DOCUMENT_POSITION_FOLLOWING;
+  },
+
   /* returns NodeList */
   getElementsByTagName: function(/* string */ name) {
     var queryFunction = function(child) {
