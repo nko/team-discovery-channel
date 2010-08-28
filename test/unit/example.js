feec418fe4c7cf1728ad514acb145acc1d5d4a00
bcoe <bencoe@gmail.com>
2010-08-28 13:28:43 -0400

    Created more tests.

--- test/unit/example.js
+++ test/unit/example.js
@@ -4,3 +4,12 @@ test("test_assertions", function() {
     equals(1, 1, "Simple assertion test.");
     equals(jQuery('#content').html(), "Not Some Content", "Assert that we get the content of a div with an ID.");
 });
+
+test("test_jquery", function() {
+    var element = $('<div></div>');
+    element.text('Hello World!');
+    element.attr('id', 'foobar');
+    $('body').append(element);
+    var test = $('#foobar');
+    equals($('#foobar').text(), "Hello World!", "Created element with jQuery added it to DOM and pulled it out.");
+});
