28301f2b29f2d810d7c221225682168c0b437553
bcoe <bencoe@gmail.com>
2010-08-28 16:30:15 -0400

    Adding more tests, because that's how I roll.

--- test/unit/example2.js
+++ test/unit/example2.js
@@ -13,3 +13,16 @@ test("test_query_2", function() {
     var test = $('#foobar');
     equals($('#foobar').text(), "Hello World!", "File 2: Created element with jQuery added it to DOM and pulled it out.");
 });
+
+test("Awesome Test", function() {
+    var element = $('<div></div>');
+    element.text('Goodbye');
+    element.attr('id', 'foobar');
+    $('body').append(element);
+    var test = $('#foobar');
+    test.click(function() {
+        $(this).text('WOOOOOOOOOO!')
+    });
+    test.trigger('click');
+    equals($('#foobar').text(), "Hello World!", "File 2: Created element with jQuery added it to DOM and pulled it out.");
+});
