4b88859d2761a70bcde68d532c50601f66a8e47e
bcoe <bencoe@gmail.com>
2010-08-28 23:08:27 -0400

    Breaking tests.

--- test/unit/example1.js
+++ test/unit/example1.js
@@ -1,7 +1,7 @@
 module('nko');
 
 test("test_assertions", function() {
-    equals(1, 1, "Simple assertion test.");
+    equals(1, 2, "Simple assertion test.");
      equals(jQuery('#content').html(), "Some Content", "Assert that we get the content of a div with an ID.");
 });
 
