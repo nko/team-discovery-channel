9e7bbf82caf7083928c7552376108f8829584a0d
bcoe <bencoe@gmail.com>
2010-08-28 22:04:27 -0400

    Building a twitter widget.

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.post('/tests/', function(req, res) {
         
         var id = crypto.createHash('md5').update(req.body.url).digest('hex');
                 
-        db.saveDoc(id, {url: req.body.url, type: 'test', twitter: req.body.twitter}, function(er, doc) {
+        db.saveDoc(id, {url: req.body.url, type: 'test'}, function(er, doc) {
             if (er.error !== 'conflict') {
                 // We're fine with conflicts; it means id already exists, but
                 // because we hash url to receive id, the url couldn't have changed
